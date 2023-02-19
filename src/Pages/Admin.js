21f14cac2fdbfa1f4a1a4609e40966afc6e65afd
Jean-Lionel <nijeanlionel@gmail.com>
2023-02-19 06:43:21 +0200

    contact create

--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -3,14 +3,15 @@ import AdminFooter from "../components/admin/AdminFooter";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ResponsiveAppBar from "../components/admin/AppBar";
+import { Box } from "@mui/material";
 
 const Admin = (props) => {
-    return ( <div>
+    return ( <Box sx={{width: '100%', overflow: 'hidden'}}>
         <ResponsiveAppBar/>
           <ToastContainer/>
           {props.children}
         <AdminFooter/>  
-    </div> );
+    </Box> );
 }
  
 export default Admin;
--- src/components/admin/contacts/ContactCreate.jsx
+++ src/components/admin/contacts/ContactCreate.jsx
@@ -1,14 +1,18 @@
-import { Box, FormControl, Input, InputLabel } from "@mui/material";
+import { Box, Button, FormControl, Input, InputLabel } from "@mui/material";
 import { useState } from "react";
+import usePostData from "../../../utility/usePostData";
 
 const ContactCreate = () => {
     const [title_en, setTitle_en] = useState("");
     const [title_fr, setTitle_fr] = useState("");
     const [content_en, setContent_en] = useState("");
     const [content_fr, setContent_fr] = useState("");
+    const { submitData} = usePostData();
 
     const handleSubmit = () => {
-        alert('Je suis en bonne du forme')
+        submitData("contacts", {
+            title_en,title_fr,content_en,content_fr
+        }, "POST");
     }
     return (<Box sx={{width: '99%'}}>
         <h6>Création des contacts</h6>
@@ -45,16 +49,21 @@ const ContactCreate = () => {
                 </div>
                 <div className="col-md-6">
                       <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-                <InputLabel htmlFor="content_fr"  >Contenue en Anglais</InputLabel>
-                <Input id="content_fr" value={content_fr}
+                <InputLabel htmlFor="content_en"  >Contenue en Anglais</InputLabel>
+                <Input id="content_en" value={content_en}
                             required
-                            onChange={(e) => (setContent_fr(e.target.value))}
+                            onChange={(e) => (setContent_en(e.target.value))}
                 ></Input>
             </FormControl>
                 </div>
             </div>
            
-            <div>
+            <div className="row">
+                <div className="offset-6  col-md-6">
+                  <Button type="submit" variant="contained">
+                    Enregistrer
+                    </Button>
+                </div>
             </div>
             
          </form>
