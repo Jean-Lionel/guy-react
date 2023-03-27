2095f36440664d2f4ef0e669726daa61b995ed2a
Jean-Lionel <nijeanlionel@gmail.com>
2023-03-27 05:34:30 +0200

    user membre

--- src/Pages/Admin/users/UserMembre.js
+++ src/Pages/Admin/users/UserMembre.js
@@ -1,7 +1,13 @@
+
+import useFetchDataWithPagination from "../../../utility/useFetchDataWithPagination";
 import Admin from "../../Admin";
 
 const UserMembre = () => {
+    const { data: users , error, paginate} = useFetchDataWithPagination('get_user_instution')
     return (<Admin>
+        <h1>Hello je suis en bonne santé</h1>
+
+        {JSON.stringify(users)}
     </Admin>);
 }
  
