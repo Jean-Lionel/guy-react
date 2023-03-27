79263c3ca5e8236e4e66a394c3437bb38c08dd2f
Jean-Lionel <nijeanlionel@gmail.com>
2023-03-27 06:43:26 +0200

    change password

--- src/Pages/Admin/users/UserMembre.js
+++ src/Pages/Admin/users/UserMembre.js
@@ -2,12 +2,18 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import useFetchDataWithPagination from "../../../utility/useFetchDataWithPagination";
+import usePostData from "../../../utility/usePostData";
 import Admin from "../../Admin";
 
 const UserMembre = () => {
-    const { data: users, error, paginate, isLoading } = useFetchDataWithPagination('get_user_instution')
+    const [matricule, setMatricule] = useState("")
+    let { data: users, error, paginate, isLoading , refreshSearch ,searchIntoDatabase } = useFetchDataWithPagination('get_member/?matricule='+matricule)
+    const { submitData } = usePostData()
     
     const [listUser, setListUser] = useState(null)
+    const [selected_input, setSelected_input] = useState(-1)
+
+    const [password, setPassword] = useState("")
 
     useEffect(() => {
         if (users?.data) {
@@ -17,6 +23,24 @@ const UserMembre = () => {
         
       }
     }, [users])
+
+
+    const changePassword = () => { 
+        if (!password) {
+            alert('Please enter the new password')
+        }
+        submitData('change_user_password', {
+            user_id: selected_input,
+            password: password
+        }, "POST");
+        refreshSearch()
+
+        setSelected_input(-1)
+    }
+
+    const search = () => {
+        searchIntoDatabase()
+    }
     
     return (<Admin>
         {isLoading && <p>Loading .........</p>}
@@ -24,11 +48,13 @@ const UserMembre = () => {
 
         <div className="row">
             <div className="offset-md-5 col-md-6">
-                <input type="text" className="form-control" placeholder="Numero Matricule"/>
+                <input type="text" className="form-control"
+                    onChange={(e) =>setMatricule(e.target.value)}
+                    placeholder="Numero Matricule" />
             </div>
 
             <div className="col-md-1">
-                <button className="btn btn-block btn-primary">Ok</button>
+                <button className="btn btn-block btn-primary" onClick={search} >Ok</button>
             </div>
         </div>
 
@@ -60,7 +86,19 @@ const UserMembre = () => {
                             </td>
                             <td>{ user.updated_at?.split('T')[0] } à { user.updated_at?.split('T')[1].split('.')[0] }</td>
                             <td>
-                            <button>Change de Mot de passe</button>
+                               
+                                {
+                                    (selected_input === user.id) &&
+                                    <>
+                                        <input type="text" onChange={(e) => setPassword(e.target.value)} placeholder="Change de Mot de passe" />
+                                        <button onClick={changePassword}>Change</button>
+                                    </>
+                                }
+                                {
+                                    (selected_input !== user.id) &&
+                                    <button onClick={() => setSelected_input(user.id)}>edit</button>
+                                }
+                            
                             </td>
                         </tr>
                     
