700afffe1c69fbdd9622bdf52bd175c929b16259
Jean-Lionel <nijeanlionel@gmail.com>
2023-03-27 05:52:03 +0200

    foramatage de date avec amanyanga

--- src/Pages/Admin/users/UserMembre.js
+++ src/Pages/Admin/users/UserMembre.js
@@ -1,13 +1,69 @@
 
+import { useState } from "react";
+import { useEffect } from "react";
 import useFetchDataWithPagination from "../../../utility/useFetchDataWithPagination";
 import Admin from "../../Admin";
 
 const UserMembre = () => {
-    const { data: users , error, paginate} = useFetchDataWithPagination('get_user_instution')
-    return (<Admin>
-        <h1>Hello je suis en bonne santé</h1>
+    const { data: users, error, paginate, isLoading } = useFetchDataWithPagination('get_user_instution')
+    
+    const [listUser, setListUser] = useState(null)
 
-        {JSON.stringify(users)}
+    useEffect(() => {
+        if (users?.data) {
+            setListUser(users?.data.data)
+        }
+      return () => {
+        
+      }
+    }, [users])
+    
+    return (<Admin>
+        {isLoading && <p>Loading .........</p>}
+        {error && JSON.stringify(error)}
+
+        <table className="table table-hover table-sm table-responsive ">
+            <thead className="table-dark">
+                <tr>
+                    <th>ID</th>
+                    <th>Nom et Prénom </th>
+                    <th>Téléphone</th>
+                    <th>Matricule</th>
+                    <th>Updated at</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+               
+                {
+                    
+                    listUser && listUser.map((user, index) => (
+
+                        <tr key={index} style={{textAlign: 'left'}}>
+                           
+                                <td>{index + 1}</td>
+                                <td >{user?.name}</td>
+                                
+                                <td >{user?.telephone}</td>
+                                <td>
+                                {user?.numero_matricule}
+                            </td>
+                            <td>{ user.updated_at?.split('T')[0] } à { user.updated_at?.split('T')[1].split('.')[0] }</td>
+                            <td>
+                            <button>Change de Mot de passe</button>
+                            </td>
+                        </tr>
+                    
+                    
+                ))
+            }
+
+            </tbody>
+        </table>
+       
+        {paginate()}
+
+        { paginate()}
     </Admin>);
 }
  
